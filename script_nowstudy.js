e587ba219e9a5b0c182ae3d6ec9db530c93b2f86
ItingT <88220140+ItingT@users.noreply.github.com>
2021-08-04 16:03:56 +0800

    Update script_nowstudy.js

--- script_nowstudy.js
+++ script_nowstudy.js
@@ -1,23 +1,25 @@
 
-              function prohibitpreviouspage(){
+function prohibitpreviouspage(){
 
-                if(navigator.userAgent.indexOf('Firefox') != -1 && parseFloat(navigator.userAgent.substring(navigator.userAgent.indexOf('Firefox') + 8)) >= 3.6 ){
+if(navigator.userAgent.indexOf('Firefox') != -1 && parseFloat(navigator.userAgent.substring(navigator.userAgent.indexOf('Firefox') + 8)) >= 3.6 ){
+//Firefox
+  setTimeout("fn_forward()",1);
+  window.history.go(1);
+}else{ //IE.Chrome.Edge
+  window.history.forward();
+}
+}
 
-                //Firefox
-                setTimeout("fn_forward()",1);
-                window.history.go(1);
-                }else{ //IE.Chrome.Edge
-                window.history.forward();
-                }
-                }
-              function fn_forward() {
-                history.forward();
-                setTimeout("fn_forward()",1);
-                }
-              function MS(){
-                return window.location.assign("https://itingt.github.io/test1/school_info_g")
-                }
-              function N(){
-                return window.location.assign("https://itingt.github.io/test1/end_page")
-                }
+function fn_forward() {
+  history.forward();
+  setTimeout("fn_forward()",1);
+}
+              
+function MS(){
+  return window.location.assign("https://itingt.github.io/test1/school_info_g")
+}
+
+function N(){
+  return window.location.assign("https://itingt.github.io/test1/end_page")
+}
 
